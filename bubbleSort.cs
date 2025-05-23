e127754a0cfdbd9570d94f14cc9f3ce47ff40197
pi128 <77074839+pi128@users.noreply.github.com>
2025-05-23 13:26:31 -0400

    trying new stuff

---
+++ bubbleSort.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+class bubbleSort
+{
+    static void Main()
+    List<int> list = new List<int>();
+    Console.WriteLine("Enter numbers for the list (type a non-number to stop):");
+
+    
+    while(true){
+        Console.writeLine("Enter a number: ")
+        string input = Console.ReadLine();
+
+        if (int.Tryparse(input, out num)){
+            list.Add(num);
+        } else {
+            Console.WriteLine("Invalid input or non number. Shutting input");
+        }
+    }
+
+    foreach (int num in list) {
+        Console.WriteLine(num);
+    }
+
+
+    
+}
---
+++ sorts/bubbleSort.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter something: ")
+	// 1. Prompt user for input
+
+	// 2. Read and parse input
+
+	// 3. Call bubbleSort function
+
+	// 4. Print sorted result
+}
+
+func bubbleSort(arr []int) {
+	// Implement bubble sort algorithm here
+}
